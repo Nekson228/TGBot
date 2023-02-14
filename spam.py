f80a2bcda73c6ac1b06678fa0846979d04fa8e89
neepaw <bangerwockeez@gmail.com>
2023-02-14 23:59:57 +0300

    0.1.1: spam.py upgraded

--- spam.py
+++ spam.py
@@ -3,7 +3,7 @@ import json
 import sys
 
 cc = '7'
-phone = '9825744661'
+phone = '9024925012'
 RED = '\033[31m'
 ENDC = '\033[0m'
 
@@ -13,28 +13,24 @@ def get_response(url, method: str, headers: dict = None, parameters: dict = None
         response = requests.request(method, url, headers=headers, params=parameters, data=data)
         if response.status_code == 200:
             return response
-        elif response.status_code == 401:
-            print(f'{RED}{response.text}{ENDC}')
-        else:
-            print(f'{RED}{response.status_code}{ENDC}')
+        print(f'{RED}{response.status_code}{ENDC}')
+        return None
     except requests.exceptions.ConnectionError:
         print(f'{RED}No internet connetion.{ENDC}')
-    sys.exit()
+        sys.exit()
 
 
-reqs = json.load(open('apidata.json', 'r'))
+with open('apidata.json', 'r') as file:
+    reqs = json.load(file)
 
-for i in range(len(reqs['sms']['multi'])):
-    new_req = reqs['sms']['multi'][i]
-    params = new_req.get('param', None)
+for new_req in reqs['sms']['multi']:
+    params = new_req.get('params', None)
+    headers = new_req.get('headers', None)
 
     data_req = new_req.get('data', None)
     if data_req:
         data_req[list(data_req.keys())[0]] = data_req[list(data_req.keys())[0]].format(cc=cc, target=phone)
 
     print(new_req['method'], new_req['url'], params, data_req)
-    try:
-        response = get_response(new_req['url'], new_req['method'], parameters=params, data=data_req)
-    except:
-        print('POHUY PLASHEM')
-    print(response)
+    response = get_response(new_req['url'], new_req['method'], parameters=params, data=data_req, headers=headers)
+    print(response if response else f"{RED}Got no response :({ENDC}")
